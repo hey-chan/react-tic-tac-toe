f9e8fea381d4415e2e53c35336f57e7eadb8e86f
hey-chan <liumatthew8@gmail.com>
2021-10-25 11:02:25 +1100

    determing winner calculation

--- src/index.js
+++ src/index.js
@@ -30,6 +30,12 @@ class Board extends React.Component {
   handleClick(i){
     const squares = this.state.squares.slice();
 
+    // We can change Board handleClick function to return early by ignoring a click if someone has won a game or Square is already filled
+    if (calculateWinner(squares) || squares[i]){
+      return;
+    }
+
+
     // Changes from X and O from each click
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
@@ -54,7 +60,14 @@ class Board extends React.Component {
 
   // Will display text indicating player's turn
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = 'Next player' + (this.state.xIsNext ? 'X' : 'O')
+    }
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
@@ -93,6 +106,29 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+// Determining winner based on possition
+// Returns 'x', 'o' or null as appropriate
+function calculateWinner(squares){
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ];
+
+  for (let i=0; i < lines.length; i++){
+    const [a,b,c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares [a] === squares[c]){
+      return squares [a]
+    }
+  }
+  return null
 }
 
 // ========================================
